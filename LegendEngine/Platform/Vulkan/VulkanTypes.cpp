1b2715a9de94587c820e955b7a2b14e9a3bf3226
SawtoothGabe <101486581+SawtoothGabe@users.noreply.github.com>
2026-04-06 18:18:59 -0500

    Conversion funcs

--- LegendEngine/Platform/Vulkan/VulkanTypes.cpp
+++ LegendEngine/Platform/Vulkan/VulkanTypes.cpp
@@ -32,6 +32,7 @@ namespace le
             case Format::R8G8: return VK_FORMAT_R8G8_SRGB;
             case Format::R8G8B8: return VK_FORMAT_R8G8B8_SRGB;
             case Format::R8G8B8A8: return VK_FORMAT_R8G8B8A8_SRGB;
+            case Format::R32G32B32_FLOAT: return VK_FORMAT_R32G32B32_SFLOAT;
         }
 
         LE_ASSERT(false, "Unknown image format");
@@ -41,5 +42,87 @@ namespace le
     vk::Format VulkanTypes::GetVkFormat(const Format format)
     {
         return static_cast<vk::Format>(GetFormat(format));
+    }
+
+    vk::ShaderStageFlagBits VulkanTypes::GetShaderStageFlag(ShaderStageFlags stage)
+    {
+        switch (stage)
+        {
+            case ShaderStageFlags::VERTEX: return vk::ShaderStageFlagBits::eVertex;
+            case ShaderStageFlags::FRAGMENT: return vk::ShaderStageFlagBits::eFragment;
+            case ShaderStageFlags::ALL: return vk::ShaderStageFlagBits::eAll;
+        }
+
+        LE_ASSERT(false, "Unknown shader stage");
+        return vk::ShaderStageFlagBits::eAll;
+    }
+
+    vk::ShaderStageFlagBits VulkanTypes::GetShaderStageFlags(ShaderStageFlags stage)
+    {
+        return static_cast<vk::ShaderStageFlagBits>(stage);
+    }
+
+    vk::VertexInputRate VulkanTypes::GetVertexInputRate(const InputRate inputRate)
+    {
+        switch (inputRate)
+        {
+            case InputRate::VERTEX: return vk::VertexInputRate::eVertex;
+            case InputRate::INSTANCE: return vk::VertexInputRate::eInstance;
+        }
+
+        LE_ASSERT(false, "Unknown input rate");
+        return vk::VertexInputRate::eVertex;
+    }
+
+    vk::DescriptorType VulkanTypes::GetDescriptorType(DescriptorType type)
+    {
+        switch (type)
+        {
+            case DescriptorType::UNIFORM_BUFFER: return vk::DescriptorType::eUniformBuffer;
+            case DescriptorType::STORAGE_BUFFER: return vk::DescriptorType::eStorageBuffer;
+            case DescriptorType::COMBINED_IMAGE_SAMPLER: return vk::DescriptorType::eCombinedImageSampler;
+        }
+
+        LE_ASSERT(false, "Unknown descriptor type");
+        return vk::DescriptorType::eUniformBuffer;
+    }
+
+    vk::Filter VulkanTypes::GetFilter(Filter filter)
+    {
+        switch (filter)
+        {
+            case Filter::NEAREST: return vk::Filter::eNearest;
+            case Filter::LINEAR: return vk::Filter::eLinear;
+        }
+
+        LE_ASSERT(false, "Unknown sampler filter mode");
+        return vk::Filter::eNearest;
+    }
+
+    vk::SamplerAddressMode VulkanTypes::GetSamplerAddressMode(const AddressMode addressMode)
+    {
+        switch (addressMode)
+        {
+            case AddressMode::REPEAT: return vk::SamplerAddressMode::eRepeat;
+            case AddressMode::MIRRORED_REPEAT: return vk::SamplerAddressMode::eMirroredRepeat;
+            case AddressMode::CLAMP_TO_EDGE: return vk::SamplerAddressMode::eClampToEdge;
+            case AddressMode::CLAMP_TO_BORDER: return vk::SamplerAddressMode::eClampToBorder;
+        }
+
+        LE_ASSERT(false, "Unknown sampler addressMode");
+        return vk::SamplerAddressMode::eRepeat;
+    }
+
+    vk::BorderColor VulkanTypes::GetBorderColor(const BorderColor borderColor)
+    {
+        switch (borderColor)
+        {
+            case BorderColor::TRANSPARENT_BLACK: return vk::BorderColor::eIntTransparentBlack; break;
+            case BorderColor::OPAQUE_BLACK: return vk::BorderColor::eIntOpaqueBlack; break;
+            case BorderColor::OPAQUE_WHITE: return vk::BorderColor::eIntOpaqueWhite; break;
+        }
+
+        LE_ASSERT(false, "Unknown border color");
+        return vk::BorderColor::eIntOpaqueBlack;
     }
 }
